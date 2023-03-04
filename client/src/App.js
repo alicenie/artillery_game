5b6f494671e6133942e12f29733637e32310a0c2
alicenie <fnie9421@gmail.com>
2023-03-04 18:05:15 -0500

    set 2 players per room and wait for both players to start

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useState } from "react";
+
+const socket = io.connect("http://localhost:8080");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [room, setRoom] = useState("");
+  const [isTurn, setIsTurn] = useState(false);
+  const [side, setSide] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+
+  const handleSendMessage = () => {
+    socket.emit("send_message", { message, room });
+  };
+
+  // only join the room once, on mount
+  useEffect(() => {
+    console.log("useEffect");
+    socket.on("connect", () => {
+      console.log("client connect to socket");
+      // socket.emit("join_room");
+    });
+  }, []);
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data);
+    });
+
+    socket.on("joined_room", (room) => {
+      console.log("joined room", room);
+      setRoom(room);
+    });
+
+    socket.on("start_game", (data) => {
+      setIsTurn(data.isTurn);
+      setSide(data.side);
+      console.log("start game: ", data.isTurn, data.side);
+    });
+  }, [socket]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        placeholder="message here..."
+        onChange={(e) => {
+          setMessage(e.target.value);
+        }}
+      />
+      <button onClick={handleSendMessage} disabled={!isTurn}>
+        Send Message
+      </button>
+      <p>Message received: {messageReceived}</p>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -15,6 +15,46 @@ const io = new Server(server, {
   },
 });
 
+let roomCounter = 0;
+const playersPerRoom = 2;
+
+io.on("connection", (socket) => {
+  console.log("User connected: ", socket.id);
+
+  // join a room
+  const rooms = io.sockets.adapter.rooms;
+  // get the last room
+  const lastRoom = rooms.get(`room-${roomCounter}`);
+  if (!lastRoom) {
+    // if no room exists, create the first room
+    socket.join(`room-${roomCounter}`);
+  } else {
+    // check if the last room is occupied by two players
+    const numPlayers = lastRoom ? lastRoom.size : 0;
+    if (numPlayers < playersPerRoom) {
+      // Add the player to the last room
+      socket.join(`room-${roomCounter}`);
+      // start the game
+      socket.emit("start_game", { isTurn: false, side: "right" });
+      socket
+        .to(`room-${roomCounter}`)
+        .emit("start_game", { isTurn: true, side: "left" });
+    } else {
+      // Create a new room and add the player to that room
+      roomCounter += 1;
+      socket.join(`room-${roomCounter}`);
+    }
+  }
+  console.log("rooms", rooms);
+
+  socket.emit("joined_room", `room-${roomCounter}`);
+
+  socket.on("send_message", (data) => {
+    console.log(data.room);
+    socket.to(data.room).emit("receive_message", data.message);
+  });
+});
+
 server.listen(8080, () => {
   console.log("SERVER IS RUNNING");
 });
